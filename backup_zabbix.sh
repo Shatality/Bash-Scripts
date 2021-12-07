82798c0ad0eb78dae59c62c56eb527623ef11bf8
Vadim Shatalov <djshatalov@gmail.com>
2021-12-07 16:29:02 +0300

    mkdir & chmod

--- backup_zabbix.sh
+++ backup_zabbix.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Создаем каталог Backup со вложенными каталогами и назначаем права
+sudo mkdir -p /backup/zabbix_server/etc_zabbix/ /backup/zabbix_server/usr_share_zabbix && chmod -R 777 /backup
+
 #Копируем конфигурационные файлы Zabbix
 sudo cp -a /etc/zabbix/ /backup/zabbix_server/etc_zabbix
 sudo cp -a /usr/share/zabbix/ /backup/zabbix_server/usr_share_zabbix
