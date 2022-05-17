0547a877ea852c6ca692d3b0d606b779eda171c5
Vadim Shatalov <djshatalov@gmail.com>
2022-05-17 11:59:03 +0300

    master pos

--- mysqlback.sh
+++ mysqlback.sh
@@ -5,6 +5,6 @@
   mkdir -p $s
     for t in $(mysql --defaults-extra-file=/etc/my.cnf --skip-column-names $s -e "SHOW TABLES");
     do
-    mysqldump --master-data=2 --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers $s | gzip -1 > $s/$t.gz;
+    mysqldump --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers $s | gzip -1 > $s/$t.gz;
     done
 done
