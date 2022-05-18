163c04e29dc8590e6ed118c8ef6b556f1b4cd93b
Vadim Shatalov <djshatalov@gmail.com>
2022-05-18 15:10:50 +0300

    mysql slave sh

--- mysql_slave_install.sh
+++ mysql_slave_install.sh
@@ -13,23 +13,26 @@ mysql_secure_installation
 # Отключаем firewalld
 systemctl stop firewalld
 systemctl disable firewalld
-# Меняем server-id и влючаем bin-log
-echo "server-id=1" >> /etc/my.cnf
-echo "log-bin=mysql-bin" >> /etc/my.cnf
-# Заходим в MySQL с новым паролем
-cat > ~/.my.cnf << EOF
+# НАСТРОЙКА SLAVE
+# Добавляем server-id
+echo "server-id=2" >> /etc/my.cnf
+echo "show_compatibility_56=1" >> /etc/my.cnf
+# Добавляем данные клиента чтобы входить без запроса пароля
+cat > ~/.my.cnf <<EOF
 [client]
 user=root
 password=JackBob5000@!
 EOF
-# НАСТРОЙКА MASTER
-# Создаём пользователя для реплики
-mysql -e"CREATE USER repl@'%' IDENTIFIED WITH 'mysql_native_password' BY 'oTUSlave#2020';"
-# Даём ему права на репликацию
-mysql -e"GRANT REPLICATION SLAVE ON *.* TO repl@'%';"
 # Перезапускаем сервер
 systemctl restart mysqld
 # Добавляем MySQL в автозагрузку
 systemctl enable mysqld
-# Смотрим статус Мастера
-mysql -e"SHOW MASTER STATUS;"
+MasterUser="repl"
+MasterPassword="oTUSlave#2020"
+read -p "Enter MASTER HOST:" MasterHost
+read -p "Enter MASTER LOG FILE:" MasterLogFile
+read -p "Enter MASTER LOG POS:" MasterLogPos
+mysql -e"CHANGE MASTER TO MASTER_HOST='$MasterHost', MASTER_USER='$MasterUser', MASTER_PASSWORD='$MasterPassword', MASTER_LOG_FILE='$MasterLogFile', MASTER_LOG_POS=$MasterLogPos;"
+mysql -e"START SLAVE;"
+mysql -e"SHOW SLAVE STATUS\G;"
+
